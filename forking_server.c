da0b2c7d4f46f8ce36d53691b74008b88871207a
TakingTheL <yliu6@stuy.edu>
2016-02-11 11:59:36 -0500

    hope this actually works

--- forking_server.c
+++ forking_server.c
@@ -12,10 +12,31 @@ static void sighandler(int signo) {
 }
 
 int main() {
+  int to_client;
+  int from_client;
+  while (1){
+    from_client = server_handshake( &to_client);
+
+    char buf[BUFFER_SIZE];
+    read(from_client, buf, BUFFER_SIZE);
+    
+    process(buf);
+    
+    write(to_client, buf, BUFFER_SIZE);
+  }
 }
 
 void subserver(int from_client) {
 }
 
 void process(char * s) {
+  while(*s){
+    if((*s >= 'a' && *s <= 'm') || (*s >= 'A' && *s <= 'M')){
+      *s += 13;
+    } 
+    else if((*s >= 'n' && *s <= 'z')||(*s >= 'N' && *s <= 'Z')) {
+      *s -= 13;
+    }
+    s ++;
+  }
 }
